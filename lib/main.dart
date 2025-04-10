317cf66a867071a236f3dbeb270d54f63f933db9
Shreyansh Shukla <130576066+imshreyanshshukla@users.noreply.github.com>
2025-04-11 01:39:14 +0530

    Added preferences and fixed blank screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,21 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Check if Firebase has already been initialized
+  // Initialize Firebase if not already initialized
   if (Firebase.apps.isEmpty) {
     await Firebase.initializeApp();
   }
 
-  runApp(MyApp());
+  // Check if it's the first time the app is launched
+  final prefs = await SharedPreferences.getInstance();
+  final isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
+
+  // If it's the first time, set the flag to false
+  if (isFirstLaunch) {
+    await prefs.setBool('isFirstLaunch', false);
+  }
+
+  runApp(MyApp(isFirstLaunch: isFirstLaunch));
 }
 
 class MyApp extends StatelessWidget {
